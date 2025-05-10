b4159ea249958238c4b3883cdde73cb5de394b59
eablak <esrablk9@gmail.com>
2025-05-10 16:18:20 +0300

    remove

--- Django-0-Initiation/ex00/myawesomescript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-if [ -z "$1" ]
-then
-    echo "Error: URL eksik!"
-    exit 1
-fi
-
-curl -s $1 | grep "body" |cut -d'"' -f2
--- Django-0-Starting/ex01/numbers.py
+++
@@ -1,9 +0,0 @@
-def print_numbers():
-    f = open("numbers.txt")
-    numbers = f.read()
-    numbers = numbers.split(",")
-    for num in numbers:
-        print(num)
-
-if __name__ == "__main__":
-    print_numbers()
