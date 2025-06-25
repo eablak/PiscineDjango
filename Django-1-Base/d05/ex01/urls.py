1a32cb8985f150c39facb54b91668b37e6b3ae47
eablak <esrablk9@gmail.com>
2025-06-25 17:57:08 +0300

    ex01 ok

--- Django-1-Base/d05/ex01/urls.py
+++ Django-1-Base/d05/ex01/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("ex01/django", views.django),
-    path("ex01/display", views.display),
-    path("ex01/templates", views.templates),
+    path("ex01/django", views.django, name="django"),
+    path("ex01/display", views.display, name="display"),
+    path("ex01/templates", views.templates, name="templates"),
 ]
--- Django-1-Base/d05/ex01/views.py
+++ Django-1-Base/d05/ex01/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def django(request):
-    return HttpResponse("django page")
+    return render(request, "django.html")
 
 def display(request):
-    return HttpResponse("display page")
+    return render(request, "display.html")
 
 def templates(request):
-    return HttpResponse("templates page")
+    return render(request, "templates.html")
