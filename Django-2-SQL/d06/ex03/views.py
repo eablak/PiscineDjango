dd4c54cc32586c88435e27a5b011690c002a75cf
eablak <esrablk9@gmail.com>
2025-07-15 15:17:19 +0300

    ex03 ok

---
+++ Django-2-SQL/d06/ex03/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import os
+from django.conf import settings
+import json
+from .models import Movies
+from django import db
+
+# Create your views here.
+def populate(request):
+    path = os.path.join(settings.BASE_DIR , "ex03/data/datas.json")
+    with open(path) as f:
+        movies = json.load(f)
+
+    results = []
+
+    for movie in movies:
+        try:
+            Movies.objects.create(
+                episode_nb=movie['episode_nb'],
+                title = movie["title"],
+                director=movie['director'],
+                producer=movie['producer'],
+                release_date=movie['release_date'],
+            )
+            results.append("OK")
+        except Exception as e:
+            results.append(e)
+    return HttpResponse("\n".join(str(result)) for result in results)
+
+
+def display(request):
+    
+    try:
+        movies = Movies.objects.all()
+        if not movies:
+            return HttpResponse("No data available")
+        return render(request, "ex03/display.html", {"movies":movies})
+    except Exception as e:
+        return HttpResponse(e)
