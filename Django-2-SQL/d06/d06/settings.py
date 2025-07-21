a70f92dab5b19a27e5145fee5e65a4651e105bf8
eablak <esrablk9@gmail.com>
2025-07-21 11:32:05 +0300

    registering models in admin

--- Django-2-SQL/d06/d06/settings.py
+++ Django-2-SQL/d06/d06/settings.py
@@ -77,19 +77,19 @@ WSGI_APPLICATION = 'd06.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangotraining',
-        'USER': 'djangouser',
-        'PASSWORD': 'secret',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'djangotraining',
+    #     'USER': 'djangouser',
+    #     'PASSWORD': 'secret',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 
 }
 
