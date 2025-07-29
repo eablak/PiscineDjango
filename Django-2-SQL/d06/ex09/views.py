000086017672f51c15668f0d19f12d66c80a7f26
eablak <esrablk9@gmail.com>
2025-07-29 13:11:48 +0300

    IntegrityError

--- Django-2-SQL/d06/ex09/views.py
+++ Django-2-SQL/d06/ex09/views.py
@@ -8,7 +8,7 @@ def display(request):
     try:
         planets = Planets.objects.all()
         if not planets:
-            return HttpResponse("No data available, please use the following command line before use:")
+            return HttpResponse("No data available, please use the following command line before use: python3 manage.py loaddata ex09/ex09_initial_data.json")
         return render(request, "ex09/display.html", {"planets":planets})
     except Exception as e:
         return HttpResponse(e)
