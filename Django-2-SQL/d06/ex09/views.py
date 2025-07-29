df45905b6da24b89d1b8be9af2039423033e93ef
eablak <esrablk9@gmail.com>
2025-07-29 12:44:22 +0300

    start ex09

---
+++ Django-2-SQL/d06/ex09/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Planets, People
+from django.http import HttpResponse
+
+# Create your views here.
+
+def display(request):
+    try:
+        planets = Planets.objects.all()
+        if not planets:
+            return HttpResponse("No data available, please use the following command line before use:")
+        return render(request, "ex09/display.html", {"planets":planets})
+    except Exception as e:
+        return HttpResponse(e)
