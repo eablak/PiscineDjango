e0ebaa97ab9493f78332f5db5957ccc082aa52e6
eablak <esrablk9@gmail.com>
2025-07-31 09:04:06 +0300

    fix error (allow homeworld_id null)

--- Django-2-SQL/d06/ex09/models.py
+++ Django-2-SQL/d06/ex09/models.py
@@ -12,8 +12,8 @@ class Planets(models.Model):
     rotation_period = models.IntegerField(null=True)
     surface_water = models.FloatField(null=True)
     terrain = models.CharField(null=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
 
     def __str__(self):
         return self.name
@@ -22,15 +22,15 @@ class Planets(models.Model):
 class People(models.Model):
     
     name = models.CharField(max_length=64, null=False)
-    birth_year = models.CharField(max_length=32, null=False)
+    birth_year = models.CharField(max_length=32, null=True)
     gender = models.CharField(max_length=32, null=True)
     eye_color = models.CharField(max_length=32, null=True)
     hair_color = models.CharField(max_length=32, null=True)
     height = models.IntegerField(null=True)
     mass = models.FloatField(null=True)
-    homeworld = models.ForeignKey(Planets, to_field='name', on_delete=models.CASCADE, null=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    homeworld = models.ForeignKey(Planets, on_delete=models.CASCADE, null=True)
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
 
     def __str__(self):
         return self.name
--- Django-2-SQL/d06/ex09/views.py
+++ Django-2-SQL/d06/ex09/views.py
@@ -7,8 +7,14 @@ from django.http import HttpResponse
 def display(request):
     try:
         planets = Planets.objects.all()
-        if not planets:
-            return HttpResponse("No data available, please use the following command line before use: python3 manage.py loaddata ex09/ex09_initial_data.json")
-        return render(request, "ex09/display.html", {"planets":planets})
+        peoples = People.objects.all().order_by("name")
+
+        context = {"planets":planets, "peoples":peoples}
+        
+        if not planets or not peoples:
+            return HttpResponse("No data available, please use the following command line before use: python3 manage.py dbshell" \
+            "ALTER TABLE ex09_people ALTER COLUMN homeworld_id DROP NOT NULL; " \
+            "python3 manage.py loaddata ex09/ex09_initial_data.json")
+        return render(request, "ex09/display.html", context)
     except Exception as e:
         return HttpResponse(e)
