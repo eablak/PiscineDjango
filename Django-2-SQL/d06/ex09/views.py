5845e27cae1fba3de1d5f52a066256e80616ca4c
eablak <esrablk9@gmail.com>
2025-08-02 22:09:43 +0300

    update

--- Django-2-SQL/d06/ex09/views.py
+++ Django-2-SQL/d06/ex09/views.py
@@ -5,16 +5,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def display(request):
-    try:
-        planets = Planets.objects.all()
-        peoples = People.objects.all().order_by("name")
 
-        context = {"planets":planets, "peoples":peoples}
+    try:
+        peoples = People.objects.all().order_by("name")
+        context = {"peoples":peoples}
         
-        if not planets or not peoples:
-            return HttpResponse("No data available, please use the following command line before use: python3 manage.py dbshell" \
-            "ALTER TABLE ex09_people ALTER COLUMN homeworld_id DROP NOT NULL; " \
-            "python3 manage.py loaddata ex09/ex09_initial_data.json")
+        if not peoples:
+            return HttpResponse("No data available, please use the following command line before use: python3 manage.py loaddata ex09/ex09_initial_data.json")
         return render(request, "ex09/display.html", context)
     except Exception as e:
         return HttpResponse(e)
