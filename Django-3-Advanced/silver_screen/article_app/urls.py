91417dffc761427db15ad68aa49703e4e554338e
eablak <esrablk9@gmail.com>
2025-08-16 20:58:48 +0300

    publications & detail

--- Django-3-Advanced/silver_screen/article_app/urls.py
+++ Django-3-Advanced/silver_screen/article_app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("article", views.ArticleView.as_view()),
+    path("articles", views.ArticleView.as_view()),
     path("", views.HomeView.as_view(), name="home"),
     path("login", views.LoginView.as_view(), name="login"),
+    path("publications", views.PublicationsView.as_view()),
+    path("detail/<int:pk>/", views.DetailView.as_view(), name="article_detail"),
 ]
--- Django-3-Advanced/silver_screen/article_app/views.py
+++ Django-3-Advanced/silver_screen/article_app/views.py
@@ -6,21 +6,24 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 class ArticleView(generic.ListView):
     
     model = Article
+    template_name = "article_app/article_list.html"
 
 
 class HomeView(generic.RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
-        return "/article"
+        return "/articles"
     
 
 class LoginView(LoginView):
     redirect_authenticated_user = True
+    template_name = "registiration/login.html"
 
     def get_success_url(self):
         return reverse_lazy("home")
@@ -28,3 +31,19 @@ class LoginView(LoginView):
     def form_invalid(self, form):
         messages.error(self.request, 'Invalid username or password')
         return self.render_to_response(self.get_context_data(form=form))
+    
+
+class PublicationsView(LoginRequiredMixin, generic.ListView):
+
+    model = Article
+    template_name = "article_app/publications.html"
+
+    def get_queryset(self):
+        return Article.objects.filter(
+            author=self.request.user
+        ).order_by('created')
+    
+
+class DetailView(generic.DetailView):
+    model = Article
+    template_name = "detail/detail.html"
