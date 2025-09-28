28d3f2f92261c1ddc72e48e52e55a6341e2e3d45
eablak <esrablk9@gmail.com>
2025-09-28 15:18:47 +0300

    chatrooms

--- Django-3-Final/d09/chat/views.py
+++ Django-3-Final/d09/chat/views.py
@@ -8,5 +8,5 @@ def lobby(request):
     return render(request, 'lobby.html', context={"rooms":rooms})
 
 def room(request, room_name):
-    rooms = models.ChatRoom.objects.all().values_list("name", flat=True)
-    return render(request, 'chat.html')
+    rooms = models.ChatRoom.objects.get(name=room_name)
+    return render(request, 'chat.html', context={"room_name":room_name})
