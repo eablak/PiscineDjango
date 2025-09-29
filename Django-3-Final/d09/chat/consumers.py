719b8869ec25fc1377b759b0c4b9ae568b2bfe96
eablak <esrablk9@gmail.com>
2025-09-29 09:35:56 +0300

    async & user join

--- Django-3-Final/d09/chat/consumers.py
+++ Django-3-Final/d09/chat/consumers.py
@@ -1,28 +1,36 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 from asgiref.sync import async_to_sync
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
         self.username = self.scope['user'].username
         user = self.scope['user']
 
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
         if user.is_authenticated:
-            self.accept()
+            await self.accept()
+
+            await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'user_joined',
+                'username': self.username
+            }
+            )
    
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send (
             self.room_group_name,
             {
                 'type':'chat_message',
@@ -31,12 +39,20 @@ class ChatConsumer(WebsocketConsumer):
             }
         )
 
-    def chat_message(self, event):
+
+    async def chat_message(self, event):
         message = event['message']
         username = event['username']
 
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'type':'chat',
             'message':message,
             'username': username
+        }))
+
+    async def user_joined(self, event):
+        username = event['username']
+        await self.send(text_data=json.dumps({
+            'type': 'user-joined',
+            'username': event['username']
         }))
